98c06467528c19f6a2677024216105dbd9eda1f6
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-04-07 02:18:39 +0530

    crud app - delete

--- src/components/Posts/PostDetails.js
+++ src/components/Posts/PostDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getPostById, deletePost } from '../../services/postService';
+import { Link } from 'react-router-dom';
 
 class PostDetails extends Component {
 
@@ -36,56 +37,68 @@ class PostDetails extends Component {
     return (
       <div className='container'>
         <h1>Post Details</h1>
-        <div className="list-group">
-          <div className="list-group-item">
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{this.props.post.title}</h5>
-              <small>Post Id: {this.props.post.id}</small>
+        { Object.keys(this.props.post).length !== 0? 
+          <div>
+            <div className="list-group">
+              <div className="list-group-item">
+                <div className="d-flex w-100 justify-content-between">
+                  <h5 className="mb-1">{this.props.post.title}</h5>
+                  <small>Post Id: {this.props.post.id}</small>
+                </div>
+                <p className="mb-1">
+                  {this.props.post.body}
+                </p>
+                <small>UserId: {this.props.post.userId}</small>
+                <br />
+                <button className='btn btn-primary' data-toggle='modal' data-target='#editModal'>Edit</button> &nbsp;
+                <button className='btn btn-danger' onClick={this.deleteHandler}>Delete</button>
+              </div>
             </div>
-            <p className="mb-1">
-              {this.props.post.body}
-            </p>
-            <small>UserId: {this.props.post.userId}</small>
-            <br />
-            <button className='btn btn-primary' data-toggle='modal' data-target='#editModal'>Edit</button> &nbsp;
-            <button className='btn btn-danger' onClick={this.deleteHandler}>Delete</button>
-          </div>
-        </div>
 
-        <div className="modal fade" id="editModal"
-          tabIndex={-1}
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  Update Post
-                </h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close">
-                  <span aria-hidden="true">×</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <form onSubmit={this.handleSubmit}>
-                  <input required type="text"
-                    placeholder="Enter Post Title"
-                    className='form-control' ref={(input) => this.getTitle = input} /><br />
-                  <textarea required rows="5" cols="28"
-                    placeholder="Enter Post" 
-                    className='form-control' ref={(input) => this.getContent = input} /><br />
-                  <button className='btn btn-primary' type='submit'>Save Changes</button>
-                </form>
+            <div className="modal fade" id="editModal"
+              tabIndex={-1}
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      Update Post
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <form onSubmit={this.handleSubmit}>
+                      <input required type="text"
+                        placeholder="Enter Post Title"
+                        className='form-control' ref={(input) => this.getTitle = input} /><br />
+                      <textarea required rows="5" cols="28"
+                        placeholder="Enter Post" 
+                        className='form-control' ref={(input) => this.getContent = input} /><br />
+                      <button className='btn btn-primary' type='submit'>Save Changes</button>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
+            </div>
+          : 
+          <div>
+            <div className='alert alert-success'>
+              Post has been deleted successfully!
+            </div>
+            <Link className="nav-link" to="/posts">Goto Posts Page</Link>
           </div>
-        </div>
-      </div>
+        }
+      </div> 
+      
     );
   }
 }
